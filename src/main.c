67a7a7b5518b28256ff9746d6d8e11f63e59b9af
mauromauro182 <30840779+mauromauro182@users.noreply.github.com>
2017-10-03 00:28:04 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 
 int main() {
-  int a[5];
-  int n;
+	int a[5];
+	int n;
+	int FlagNull=1;
 
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
+	scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
+	
+	n=n+1;
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
-  }
+	for (int i=1; i<n; i++) {
+		if((i%a[0])==0){
+			printf("0");
+			FlagNull=0;
+		}
+		if((i%a[1])==0){
+			printf("1");
+			FlagNull=0;
+		}
+		if((i%a[2])==0){
+			printf("2");
+			FlagNull=0;
+		}
+		if((i%a[3])==0){
+			printf("3");
+			FlagNull=0;
+		}	
+		if((i%a[4])==0){
+			printf("4");
+			FlagNull=0;
+		}
+		if(FlagNull==1){
+			printf("-");
+		}
+		printf("\n");
+		FlagNull=1;
+	}
 
-  return 0;
+	return 0;
 }
